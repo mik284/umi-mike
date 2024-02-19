99a9e13cb9fcb10c30f75579a38ea02454ff49b1
mik284 <mikeykamau222@gmail.com>
2024-02-19 15:47:48 +0300

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,22 @@
+// 运行时配置
+
+import { RunTimeLayoutConfig } from "@umijs/max";
+
+// 全局初始化数据配置，用于 Layout 用户信息和权限初始化
+// 更多信息见文档：https://umijs.org/docs/api/runtime-config#getinitialstate
+export async function getInitialState(): Promise<{ name: string }> {
+  return { name: 'Morio' };
+}
+
+export const layout:RunTimeLayoutConfig = () => {
+  return {
+    logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
+    // rightContentRender:()=>{},
+
+    menu: {
+      locale: false,
+    },
+    layout: "mix"
+    
+  };
+};
